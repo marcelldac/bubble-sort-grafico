bdf03a7dca00a73f6b5e5d916fd1928a973f821b
marcelldac <dacmarcell@proton.me>
2023-08-22 15:38:55 -0300

    refactor: apaga arq e mod estilos
    - apaga arquivos nao utilizados
    - modifica estrutura de estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,52 @@
-import "./App.css";
 import { useState, useEffect, useRef } from "react";
 
+const content = {
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  alignItems: 'center',
+  marginTop: 250,
+}
+
+const span = {
+  display: 'flex',
+  justifyContent: 'center',
+  backgroundColor: '#fff',
+  fontFamily: 'monospace',
+  marginBottom: 10,
+  paddingTop: 25,
+  width: 200,
+  height: 50,
+  borderRadius: 50,
+  fontSize: 20,
+}
+
+const input = {
+  textAlign: 'center',
+  border: 'none',
+  width: 200,
+  height: 60,
+  borderRadius: 50,
+  marginBottom: 10,
+}
+
+const button = {
+  textAlign: 'center',
+  border: 'none',
+  width: 200,
+  height: 70,
+  borderRadius: 50,
+  marginBottom: 10,
+}
+
+const mapParent = {
+  textAlign: "center",
+  marginTop:100,
+}
+
+const result = {
+  color: '#fff',
+}
 function App() {
   var [array, setArray] = useState([]);
   var [sortHistory, setSortHistory] = useState([[...array]]);
@@ -51,14 +97,14 @@ function App() {
 
   return (
     <div>
-      <div className="container">
-        <label className="label">Digite o Array: </label>
-        <input className="input" type="text" onChange={handleChange} />
-        <button className="button" onClick={() => bubblesort()}> Sort! </button>
+      <div style={content}>
+        <span style={span}>Type some number</span>
+        <input style={input} type="text" onChange={handleChange}/>
+        <button style={button} onClick={bubblesort}>Bubble Sort!</button>
       </div>
-      <div style={{ textAlign: "center", marginTop:100 }}>
+      <div style={mapParent}>
         {array.map((element) => (
-          <div className="App" style={{ height: (element + 1) * 10 }}>
+          <div style={result}>
             {element}
           </div>
         ))}
